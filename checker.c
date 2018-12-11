1c1b6951bce2673f8799ceb06eeb2a05a4025464
Ariella Grodzinsky <agrodzin@e1z2r3p23.42.us.org>
2018-12-10 16:22:57 -0800

    switch

--- checker.c
+++ checker.c
@@ -52,6 +52,105 @@ void	get_list(t_args **stack_a, int num_args, char *arg_list[])
 	}
 }
 
+/*
+int	sort_by_instructions_p2(int *args_a, int *args_b, char *str, t_arg_nums *nums)
+{
+	if (!ft_strcmp(str, "ra") && nums->num_args_a > 1)
+		rot_up(args_a, nums->num_args_a);
+	else if (!ft_strcmp(str, "rb") && nums->num_args_b > 1)
+		rot_up(args_b, nums->num_args_b);
+	else if (!ft_strcmp(str, "rr") && nums->num_args_a > 1 &&
+	nums->num_args_b > 1)
+	{
+		rot_up(args_a, nums->num_args_a);
+		rot_up(args_b, nums->num_args_b);
+	}
+	else if (!ft_strcmp(str, "rra") && nums->num_args_a > 1)
+		rot_down(args_a, nums->num_args_a);
+	else if (!ft_strcmp(str, "rrb") && nums->num_args_b > 1)
+		rot_down(args_b, nums->num_args_b);
+	else if (!ft_strcmp(str, "rrr") && nums->num_args_a > 1 &&
+	nums->num_args_b > 1)
+	{
+		rot_down(args_a, nums->num_args_a);
+		rot_down(args_b, nums->num_args_b);
+	}
+	else
+		return (0);
+	return (1);
+}
+
+int    sort_by_instructions(int *args_a, int *args_b,
+char *str, t_arg_nums *nums)
+{
+	int	check;
+
+	check = 1;
+	if (!ft_strcmp(str, "sa") && nums->num_args_a > 1)
+		ft_num_swap_individual(args_a, 0);
+	else if (!ft_strcmp(str, "sb") && nums->num_args_b > 1)
+		ft_num_swap_individual(args_b, 0);
+	else if (!ft_strcmp(str, "ss") && nums->num_args_a > 1 && nums->num_args_b > 1)
+	{
+		ft_num_swap_individual(args_a, 0);
+		ft_num_swap_individual(args_b, 0);
+	}
+	else if (!ft_strcmp(str, "pa") && nums->num_args_b > 0)
+		push_to_other(args_a, args_b, nums->num_args_a, nums->num_args_b);
+	else if (!ft_strcmp(str, "pb") && nums->num_args_a > 0)
+		push_to_other(args_b, args_a, nums->num_args_b, nums->num_args_a);
+	else
+		check = sort_by_instructions_p2(args_a, args_b, str, nums);
+	if (!ft_strcmp(str, "pa") || !ft_strcmp(str, "pb"))
+	{
+		(str[1] == 'a') ? nums->num_args_a++ : nums->num_args_a--;
+		(str[1] == 'b') ? nums->num_args_b++ : nums->num_args_b--;
+	}
+	return (check == 1 ? 1 : 0);
+}*/
+
+int		check_if_sorted(t_args *stack_a)
+{
+	int	temp;
+
+	while (stack_a)
+	{
+		if (stack_a->next)
+			temp = stack_a->next->arg;
+		if (temp < stack_a->arg)
+			return (0);
+		stack_a = stack_a->next;
+	}
+	return (1);
+}
+
+/*
+int	*check_instructions(int *args_a, t_arg_nums *nums)
+{
+	char	*str;
+	int		ret;
+	int		*args_b;
+
+	str = ft_memalloc(5);
+	args_b = ft_memalloc(nums->num_args_a * sizeof(int*) + 1);
+	while ((ret = read(1, str, 5)) != 0)
+	{
+		str[ret - 1] = '\0';
+		if (!sort_by_instructions(args_a, args_b, str, nums))
+		{
+			ft_putstr_fd("Error\n", 2);
+			free(args_b);
+			return (NULL);
+		}
+	}
+	if (check_if_sorted(args_a, nums))
+		ft_printf("OK\n");
+	else
+		ft_printf("KO\n");
+	free(args_b);
+	return (args_a);
+}*/
+
 int	main(int argc, char *argv[])
 {
 	t_args	*stack_a;
@@ -66,6 +165,10 @@ int	main(int argc, char *argv[])
 		return (0);
 	}
 	get_list(&stack_a, argc - 1, argv);
-	print_list(stack_a);
+	// print_list(stack_a);
+	if (check_if_sorted(stack_a))
+		printf("yes\n");
+	else
+		printf("no\n");
 	return (0);
 }
